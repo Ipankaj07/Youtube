9571033cc1860728a07d3c36ede9d2da06796b09
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2021-10-19 22:02:49 +0530

    Update script.js

--- YouTube/script.js
+++ YouTube/script.js
@@ -21,7 +21,8 @@ async function searchVideos() {
   let query = document.getElementById("query").value;
 
   let res = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyDx9uDNrXDdFSZbDYNenVZuiCCuDTfjilk&maxResults=20`
+    `https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyDx9uDNrXDdFSZbDYNenVZuiCCuDTfjilk&maxResults=20&safeSearch=strict&videoCaption=closedCaption&part=snippet&chart=mostPopular&regionCode=NZ`
+    // `https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyDx9uDNrXDdFSZbDYNenVZuiCCuDTfjilk&maxResults=20`   
   );
 
   let data = await res.json();
@@ -31,9 +32,11 @@ async function searchVideos() {
   appendVideos(data.items);
 }
 
+//----------------------------------------------------------
+
 // searchVideos();
 
-function appendVideos(video_data) {
+/* function appendVideos(video_data) {
   videos.innerHTML = null;
 
   video_data.forEach(({ id: { videoId } }) => {
@@ -48,6 +51,38 @@ function appendVideos(video_data) {
     div.style.marginTop = "10px";
     videos.append(div);
   });
+}
+ */
+
+//----------------------------------------------------------
+
+function appendVideos(video_data) {
+  videos.innerHTML = null;
+
+  video_data.forEach(
+    ({ id: { videoId }, snippet: { title }, snippet: { channelTitle } }) => {
+      let div = document.createElement("div");
+      div.setAttribute("class", "videoAPIBox");
+
+      let vidDiv = document.createElement("div");
+      vidDiv.innerHTML = `<iframe width="250" height="155" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+      let video_titleDiv = document.createElement("div");
+      video_titleDiv.setAttribute("class", "video_titleDiv");
+      let video_title = document.createElement("p");
+      video_title.innerText = title;
+      video_titleDiv.append(video_title);
+
+      let channel_titleDiv = document.createElement("div");
+      channel_titleDiv.setAttribute("class", "channel_titleDiv");
+      let channel_title = document.createElement("p");
+      channel_title.innerText = channelTitle;
+
+      channel_titleDiv.append(channel_title);
+      div.append(vidDiv, video_titleDiv, channel_titleDiv);
+      videos.append(div);
+    }
+  );
 }
 
 // --------------------------------------------------------------
@@ -93,3 +128,7 @@ function showVideoInBody(data) {
 }
 
 mostPopularVideo();
+
+//-------------------------------------------------------
+
+
